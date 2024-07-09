1d64f3bbd5008f51e85ca5dd1c250218af3adca4
gkluz <gkluz@datagraphic.co.uk>
2024-07-09 16:58:15 +0100

    style

--- src/components/MessageWindow.jsx
+++ src/components/MessageWindow.jsx
@@ -4,22 +4,29 @@ const Chatwindow = (  { messages } ) => {
 
 
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="flex flex-col w-1/2 h-3/4 overflow-y-auto p-4">
-        {messages.map((message) => (
-          <div
-            key={message.id}
-            className={`mb-2 p-3 rounded ${
-              message.sender === 'user'
-                ? 'bg-blue-500 text-white self-end text-right '
-                : 'bg-gray-700 text-white self-start text-left'
-            }`}
-          >
-            {message.text}
-          </div>
-        ))}
+    <div className="flex items-center justify-center">
+  <div className="flex flex-col overflow-y-auto p-2 chat">
+    {messages.map((message) => (
+      <div
+        key={message.id}
+        className={`flex mb-2 p-3 rounded items-start ${
+          message.sender === 'user'
+            ? 'bg-blue-500 text-white self-end text-right flex-row-reverse'
+            : 'bg-gray-700 text-white self-start text-left'
+        }`}
+      >
+        <img
+          className="h-10 w-10 rounded-full m-2"
+          src={message.sender === 'user' ? 'src/images/rex.png' : 'src/images/rex.png'}
+          alt=""
+        />
+        <div className="flex flex-col">
+          <div>{message.text}</div>
+        </div>
       </div>
-    </div>
+    ))}
+  </div>
+</div>
 
   )
 }
